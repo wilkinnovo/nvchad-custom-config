50a82b49a9669bdf55d6c7739f7e50a9150b692f
Wilkin Novo <w@novo.ws>
2023-09-11 14:06:45 -0400

    theme

--- chadrc.lua
+++ chadrc.lua
@@ -5,7 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
+  theme = "vscode_dark",
+  transparency = true,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
